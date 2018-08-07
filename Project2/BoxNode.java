e6dec4b6d991d02e8c531b4b70db9f3b2e9daf6a
Alex Salazar <alxanderapollo@gmail.com>
2018-08-07 16:03:04 -0400

    Add files via upload

---
+++ Project2/BoxNode.java
@@ -0,0 +1,18 @@
+/*
+ * This is The BoxNode Class 
+ * Written by Alexander Apollo Salazar
+ * 03/23/18
+ * Professor Kenneth Lord 
+ * CSC 212
+ */
+public class BoxNode extends Box {
+	
+		protected Box one; // box paramter 
+		protected BoxNode next; // node parameter 
+	
+	public BoxNode(Box userInput) {
+			this.one = userInput;
+		// TODO Auto-generated constructor stub
+	}
+
+}
